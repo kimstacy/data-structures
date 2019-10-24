078b66bfef0f0dd5922e0d2a75651dadcc72e277
Stacy Kim <stacy.kim@ucla.edu>
2019-10-23 22:00:36 -0500

    Complete functional-shared stack + queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,32 @@
-var Queue = function() {
+var Queue = function () {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queueObj = {
+    count: 0,
+    first: 0,
+    last: 0,
+    storage: {},
+  };
+  _.extend(queueObj, queueMethods);
+  return queueObj;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function (value) {
+  this.storage[this.last++] = value;
+  this.count++;
+};
 
+queueMethods.dequeue = function () {
+  if (this.count > 0) {
+    var removed = this.storage[this.first];
+    delete this.storage[this.first++];
+    this.count--;
+    return removed;
+  }
+};
+
+queueMethods.size = function () {
+  return this.count;
+};

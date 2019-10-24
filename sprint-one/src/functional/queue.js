7993c64efe9422afc8db2bdf6ef83742423d56cb
Stacy Kim <stacy.kim@ucla.edu>
2019-10-23 19:10:07 -0500

    Complete functional stack + queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,31 @@
-var Queue = function() {
+var Queue = function () {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
+  var first = 0;
+  var last = 0;
 
   // Implement the methods below
 
-  someInstance.enqueue = function(value) {
+  someInstance.enqueue = function (value) {
+    storage[last++] = value;
+    count++;
   };
 
-  someInstance.dequeue = function() {
+  someInstance.dequeue = function () {
+
+    if (count > 0) {
+      var removed = storage[first];
+      delete storage[first++];
+      count--;
+      return removed;
+    }
   };
 
-  someInstance.size = function() {
+  someInstance.size = function () {
+    return count;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,25 @@
-var Stack = function() {
+var Stack = function () {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
-  someInstance.push = function(value) {
+  someInstance.push = function (value) {
+    storage[count] = value;
+    count++;
   };
 
-  someInstance.pop = function() {
+  someInstance.pop = function () {
+    if (count > 0) {
+      count--;
+    }
+    return storage[count];
   };
 
-  someInstance.size = function() {
+  someInstance.size = function () {
+    return count;
   };
 
   return someInstance;

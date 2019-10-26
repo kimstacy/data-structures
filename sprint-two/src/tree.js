2520690ae3b3a0e824b451404775e6d19fbb7f91
Stacy Kim <stacy.kim@ucla.edu>
2019-10-26 13:04:55 -0500

    Complete tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,22 +2,37 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value)); // add value to children array
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+  // for loop will account for the 'else if''
+  for (var i = 0; i < this.children.length; i++) {
+    // new var
+    var child = this.children[i];
+    // recursion is going to take place below
+    if (child.contains(target)) {
+      return true;
+    }
+  }
+  return false;
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+treeMethods.addChild - Constant O(1)
+treeMethods.contains - Linear O(n)
+
  */

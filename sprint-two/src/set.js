9751e678c752f83388e1147e59b1754469abcca0
Stacy Kim <stacy.kim@ucla.edu>
2019-10-26 18:38:58 -0500

    Completed set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,34 @@
-var Set = function() {
+var Set = function () {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(item) {
+//method, takes any string and adds it to the set
+setPrototype.add = function (item) {
+  this._storage[item] = item;
 };
 
-setPrototype.contains = function(item) {
+// method, takes any string and returns a boolean reflecting whether it can be found in the set
+setPrototype.contains = function (item) {
+  if (this._storage[item]) {
+    return true;
+  }
+  return false;
+
 };
 
-setPrototype.remove = function(item) {
+//method, takes any string and removes it from the set, if present
+setPrototype.remove = function (item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+setPrototype.add - Constant O(1)
+setPrototype.contains - Constant O(1)
+setPrototype.remove - Constant O(1)
  */
